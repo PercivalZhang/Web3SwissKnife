f5597cc91e52ff20f7baa4cc0ab507a1412d00ff
PercivalZhang <36841424+PercivalZhang@users.noreply.github.com>
2022-09-07 16:37:53 +0800

    remove dist

--- dist/lib/LoggerFactory.d.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from "log4js";
-export declare class LoggerFactory {
-    private static instance;
-    private constructor();
-    static getInstance(): LoggerFactory;
-    getLogger(category: string): Logger;
-}
